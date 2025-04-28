6d251c0a5aaa02ad5efa205869beba616cbd332f
aschiav <anselschiavone4@gmail.com>
2025-04-28 11:21:11 -0400

    Update week1_script.R

--- week1_intro/week1_script.R
+++ week1_intro/week1_script.R
@@ -1,1 +1,18 @@
-# Week 1 Script
+# Basic Arithmetic
+2 + 2
+5 * 3
+(10 - 3) / 2
+
+# Assignment
+x <- 5
+y <- 10
+z <- x + y
+
+# Create a vector
+numbers <- c(1, 2, 3, 4, 5)
+numbers
+
+# Check environment
+ls()
+
+# Save your script by clicking 'Save' or Ctrl + S
